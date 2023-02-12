b8a0900e559703ce298b481db7d6ff9faa28a3e4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-02-12 20:37:13 +0800

    feat: add ts remark plyugin

---
+++ examples/nextjs-directive/src/lib/remark-youtube.ts
@@ -0,0 +1,75 @@
+import { visit } from "unist-util-visit";
+import type { Root } from "remark-directive";
+import type { Plugin } from "unified";
+
+export const remarkYoutube: Plugin<
+  [{ validateYoutubeLink?: boolean }],
+  Root
+> = (options = {}) => {
+  if (!options) {
+    throw Error("Options is not provided");
+  }
+
+  if (typeof options.validateYoutubeLink === "undefined") {
+    throw Error("validateYoutubeLink option is requried");
+  }
+
+  return async (tree, file) => {
+    const promises: Promise<void>[] = [];
+    visit(tree, (node) => {
+      if (
+        node.type === "textDirective" ||
+        node.type === "leafDirective" ||
+        node.type === "containerDirective"
+      ) {
+        if (node.name !== "youtube") return;
+
+        console.log(node);
+
+        const data = node.data || (node.data = {});
+        const attributes = node.attributes || {};
+        const id = attributes.id;
+
+        if (node.type === "textDirective") {
+          file.fail("Text directives for `youtube` not supported", node);
+        }
+        if (!id) {
+          file.fail("Missing video id", node);
+        }
+
+        // We fetch youtube oembed API endpoint to check whether the youtube link exist.
+
+        if (options.validateYoutubeLink) {
+          const p = fetch(
+            `https://youtube.com/oembed?url=https://www.youtube.com/watch?v=${id}`
+          )
+            .then((res) => {
+              if (res.status === 200) {
+                console.log(res);
+                data.hName = "iframe";
+                data.hProperties = {
+                  src: "https://www.youtube.com/embed/" + id,
+                  frameBorder: 0,
+                  allow: "picture-in-picture",
+                  allowFullScreen: true,
+                  className: "embed-youtube",
+                };
+              }
+            })
+            .catch((err) => console.log(err));
+          promises.push(p);
+        } else {
+          data.hName = "iframe";
+          data.hProperties = {
+            src: "https://www.youtube.com/embed/" + id,
+            frameBorder: 0,
+            allow: "picture-in-picture",
+            allowFullScreen: true,
+            className: "embed-youtube",
+          };
+        }
+      }
+    });
+    await Promise.all(promises);
+  };
+};
